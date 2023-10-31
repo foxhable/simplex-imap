c36ce065258ccbd0a89f706dff3f6b24d4696467
foxhable <foxhable@gmail.com>
2023-10-31 22:06:59 +0300

    chore: fix jest running tests

--- libs/imap-raw/jest.config.js
+++ libs/imap-raw/jest.config.js
@@ -1,11 +1,15 @@
-import presets from 'ts-jest/presets/index.js'
-
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
   transform: {
-    ...presets.jsWithTsESM.transform,
+    ".ts": [
+      "ts-jest",
+      {
+        isolatedModules: true,
+        useESM: true,
+      },
+    ],
   },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
