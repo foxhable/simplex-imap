98f6a2cfcd447aca87425325583e8f8f52a3429a
foxhable <foxhable@gmail.com>
2023-11-25 13:06:19 +0300

    feat(ten-imap): MailboxMap and Mailbox classes to inboxes method

--- libs/ten-imap/src/methods/inboxes/types.ts
+++ libs/ten-imap/src/methods/inboxes/types.ts
@@ -28,3 +28,16 @@ export interface ParsedMailbox {
   name: string
   raw: string
 }
+
+export const MAILBOX_ROLES = {
+  ALL: 'all',
+  INBOX: 'inbox',
+  ARCHIVE: 'archive',
+  DRAFTS: 'drafts',
+  FLAGGED: 'flagged',
+  JUNK: 'junk',
+  SENT: 'sent',
+  TRASH: 'trash',
+}
+
+export type MailboxRole = typeof MAILBOX_ROLES[keyof typeof MAILBOX_ROLES]
