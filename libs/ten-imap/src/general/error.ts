8ef728ce4b8dd40fbd62f74e514a0950e0404c7f
foxhable <foxhable@gmail.com>
2023-11-25 23:56:24 +0300

    feat: 'data' prop to IMAP errors

--- libs/ten-imap/src/general/error.ts
+++ libs/ten-imap/src/general/error.ts
@@ -1,9 +1,12 @@
 import { tenImapLogger } from 'logger'
 
 export class TenIMAPError extends Error {
+  public data: any[]
+
   constructor(message: string, ...data: any[]) {
     super(data ? 'Error from ten-imap lib. Detail above' : message)
     this.name = 'TenIMAPError'
+    this.data = data
     if (data) tenImapLogger.err(message, ...data)
   }
 }
