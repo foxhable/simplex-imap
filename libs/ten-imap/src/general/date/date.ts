14677bb81dd46b6d424a6993a0aa168cc1e6055d
foxhable <foxhable@gmail.com>
2024-01-16 20:55:48 +0300

    feat(ten-imap): convertToIMAPDate fn

---
+++ libs/ten-imap/src/general/date/date.ts
@@ -0,0 +1,24 @@
+import type { IMAPDate } from '../types.js'
+
+const monthsMap: {
+  [index: number]: string
+} = {
+  0: 'Jan',
+  1: 'Feb',
+  2: 'Mar',
+  3: 'Apr',
+  4: 'May',
+  5: 'Jun',
+  6: 'Jul',
+  7: 'Aug',
+  8: 'Sep',
+  9: 'Oct',
+  10: 'Nov',
+  11: 'Dec',
+}
+
+export function convertToIMAPDate(date: Date): IMAPDate {
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
+
+  return `${day}-${monthsMap[date.getMonth()]}-${date.getFullYear()}`
+}
