c4a1ab5f0117e96eb9ae001197a772567330587a
foxhable <foxhable@gmail.com>
2024-02-11 05:16:17 +0300

    chore: change jest on vitest

--- libs/ten-imap/src/general/date/date.test.ts
+++ libs/ten-imap/src/general/date/date.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from '@jest/globals'
+import { describe, expect, test } from 'vitest'
 import { convertToIMAPDate } from './date.js'
 
 describe('Date converter', () => {
