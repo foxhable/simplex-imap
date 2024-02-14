e7f4cc4d8019e88d2ace6bed7a628a9dd1e62b73
foxhable <foxhable@gmail.com>
2024-02-14 04:50:08 +0300

    feat(ten-imap): search method

--- libs/ten-imap/src/main.ts
+++ libs/ten-imap/src/main.ts
@@ -7,6 +7,7 @@ import { mailboxes } from './methods/mailboxes/mailboxes.js'
 import { login } from './methods/login/login.js'
 import { select } from './methods/select/select.js'
 import { unselect } from './methods/unselect/unselect.js'
+import { search } from './methods/search/search.js'
 
 export default class TenIMAP extends IMAP {
   public selectedMailbox: Mailbox | null = null
@@ -15,6 +16,7 @@ export default class TenIMAP extends IMAP {
   public login = login
   public select = select
   public unselect = unselect
+  public search = search
 
   constructor(config: IMAPConfig) {
     super(config)
