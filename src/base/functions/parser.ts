3d566a447acc1f60ef79c14bbe75a20cb4dfe368
foxhable <foxhable@gmail.com>
2024-09-21 16:48:32 +0300

    refactor: move all from package folders into one 'src' folder

---
+++ src/base/functions/parser.ts
@@ -0,0 +1,53 @@
+import type { IMAPResponseLine, IMAPResult, IMAPTag, ResponseCode, ResponseStatus } from '@/base/types/index.js'
+import { RESPONSE_CODES, RESPONSE_STATUSES } from '@/base/types/index.js'
+import { RawIMAPError } from '@/base/general/error.js'
+
+const TAG_REGEX_PART = '([\\d*]+)'
+const STATUS_REGEX_PART = `(${Object.values(RESPONSE_STATUSES).join('|')})`
+const CODE_REGEX_PART = `(?:\\[(${Object.values(RESPONSE_CODES).join('|')})] )?`
+const BODY_REGEX_PART = '(.*)'
+export const IMAP_RESULT_REGEX = new RegExp(`(?:\r\n)?${TAG_REGEX_PART} ${STATUS_REGEX_PART} ${CODE_REGEX_PART}${BODY_REGEX_PART}\r\n$`)
+export const IMAP_RESPONSE_LINE_REGEX = new RegExp(`([*+]) (.+)`)
+
+export function parseIMAPResponse(data: string): IMAPResult {
+  const match = data.match(IMAP_RESULT_REGEX)
+
+  if (!match) {
+    throw new RawIMAPError('Data doesnt match to regex pattern', { data })
+  }
+
+  const response = data.replace(match[0], '')
+  const responseLines = response
+    .split('\r\n')
+    .filter(Boolean)
+    .map(line => [ line, line.match(IMAP_RESPONSE_LINE_REGEX) ])
+    .map<IMAPResponseLine>(([ line, lineMatch ]) => {
+      if (!lineMatch) {
+        throw new RawIMAPError('Response line doesnt match to regex pattern',
+          {
+            line,
+            response,
+          },
+        )
+      }
+
+      return {
+        tag: lineMatch[1] as IMAPTag,
+        body: lineMatch[2],
+        raw: lineMatch[0],
+      }
+    })
+
+  return {
+    tag: match[1],
+    status: match[2] as ResponseStatus,
+    ok: (match[2] as ResponseStatus) === 'OK',
+    code: match[3] as ResponseCode || null,
+    raw: match[0].replaceAll('\r\n', ''),
+    body: match[4],
+    response: {
+      lines: responseLines,
+      raw: response,
+    },
+  }
+}
---
+++ src/main/methods/select/select.ts
@@ -0,0 +1,90 @@
+import type { SelectMethodConfig, SelectParse } from './types.js'
+import { IMAP_STATUSES, type IMAPResponseLine } from '@/base/types/index.js'
+import TenIMAP from '@/main/main.js'
+import { TenIMAPError } from '@/main/general/error.js'
+import type { MailboxFlag } from '@/main/classes/Mailbox/types.js'
+import { MAILBOX_FLAGS } from '@/main/classes/Mailbox/types.js'
+import { SelectedMailbox } from '@/main/classes/Mailbox/SelectedMailbox.js'
+
+export async function select<TConfig extends SelectMethodConfig>(
+  this: TenIMAP,
+  mailbox: string,
+  config?: TConfig,
+): Promise<TConfig extends {
+  onlyParse: infer ParseOption
+} ? ParseOption extends true ? SelectParse : SelectedMailbox : SelectedMailbox> {
+  await this._waitStatus(IMAP_STATUSES.READY)
+
+  const res = await this.send(config?.readonly ? 'EXAMINE' : 'SELECT', { mailbox })
+
+  if (!res.ok) {
+    this.selectedMailbox = null
+    throw new TenIMAPError(res.body, { res })
+  }
+
+  const parse = parseSelect(res.response.lines)
+  const selectedMailbox = new SelectedMailbox(this, { name: mailbox, mailbox: config?.mailbox })
+
+  this.selectedMailbox = selectedMailbox
+
+  // @ts-expect-error TS2322 fix later
+  return config?.onlyParse ? parse : selectedMailbox
+}
+
+const FLAGS_LIST = Object.values(MAILBOX_FLAGS).map(i => `\\${i}`).join('|')
+const FLAGS_REGEX = new RegExp(`FLAGS \\(((?:(?:${FLAGS_LIST}) ?)+)\\)`)
+const PERMANENT_FLAGS_REGEX = new RegExp(`OK \\[PERMANENTFLAGS \\(((?:(?:${FLAGS_LIST}) ?)+)\\)]`)
+const EXISTS_REGEX = /(\d+) EXISTS/
+const RECENT_REGEX = /(\d+) RECENT/
+const UNSEEN_REGEX = /OK \[UNSEEN (\d+)]/
+const UID_REGEX = /OK \[UIDVALIDITY (\d+)]/
+const NEXT_UID_REGEX = /OK \[UIDNEXT (\d+)]/
+
+function parseSelect(lines: IMAPResponseLine[]): SelectParse {
+  return lines.reduce((result, item) => {
+    switch (true) {
+      case PERMANENT_FLAGS_REGEX.test(item.body): {
+        const match = item.body.match(PERMANENT_FLAGS_REGEX)
+        result.permanentFlags = match![1].split(' ') as MailboxFlag[]
+        break
+      }
+      case FLAGS_REGEX.test(item.body): {
+        const match = item.body.match(FLAGS_REGEX)
+        result.flags = match![1].split(' ') as MailboxFlag[]
+        break
+      }
+      case EXISTS_REGEX.test(item.body): {
+        const match = item.body.match(EXISTS_REGEX)
+        result.messageCounts.exists = Number(match![1])
+        break
+      }
+      case RECENT_REGEX.test(item.body): {
+        const match = item.body.match(RECENT_REGEX)
+        result.messageCounts.recent = Number(match![1])
+        break
+      }
+      case UNSEEN_REGEX.test(item.body): {
+        const match = item.body.match(UNSEEN_REGEX)
+        result.messageCounts.unseen = Number(match![1])
+        break
+      }
+      case UID_REGEX.test(item.body): {
+        const match = item.body.match(UID_REGEX)
+        result.uid = Number(match![1])
+        break
+      }
+      case NEXT_UID_REGEX.test(item.body): {
+        const match = item.body.match(NEXT_UID_REGEX)
+        result.uidNext = Number(match![1])
+        break
+      }
+
+      case !PERMANENT_FLAGS_REGEX.test(item.body): {
+        result.permanentFlags = null
+        break
+      }
+    }
+
+    return result
+  }, { messageCounts: {} } as SelectParse)
+}
