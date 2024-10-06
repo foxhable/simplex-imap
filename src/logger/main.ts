07e05f0f6458020a35467fdd9bb9ad9c40ddcebf
foxhable <foxhable@gmail.com>
2024-10-06 14:15:18 +0300

    refactor: rewrite logger using typescript

--- src/logger/main.ts
+++ src/logger/main.ts
@@ -1,3 +1,8 @@
+interface LoggerConfig {
+  prefix: string
+  initLogLevel?: LogLevel
+}
+
 export const LOG_LEVELS = {
   NONE: 'none',
   ERR: 'err',
@@ -6,45 +11,36 @@ export const LOG_LEVELS = {
   ALL: 'all',
 }
 
-/**
- * @param {LogLevel} logLevel
- * @param {LogLevel[]} list
- * */
-function isLogLevelOneOf(logLevel, list) {
+export type LogLevel = (typeof LOG_LEVELS)[keyof typeof LOG_LEVELS]
+
+function isLogLevelOneOf(logLevel: LogLevel, list: LogLevel[]) {
   return list.map((i) => logLevel.includes(i)).includes(true)
 }
 
-export function createLogger(config) {
-  /**
-   * @param {string} text
-   * @return {string}
-   * */
-  const addPrefix = (text) => (config.prefix ? `[${config.prefix}] ${text}` : text)
+export function createLogger(config: LoggerConfig) {
+  const addPrefix = (text: string) => (config.prefix ? `[${config.prefix}] ${text}` : text)
 
-  /** @type {Logger}  */
-  const logger = {
+  return {
     logLevel: config.initLogLevel || LOG_LEVELS.NONE,
-    setLogLevel(newLevel) {
+    setLogLevel(newLevel: LogLevel) {
       this.logLevel = newLevel
     },
-    err(text, ...info) {
+    err(text: string, ...info: any[]) {
       if (!isLogLevelOneOf(this.logLevel, [LOG_LEVELS.ALL, LOG_LEVELS.ERR])) return
 
       console.error(addPrefix(text), ...info)
     },
-    log(text, ...info) {
+    log(text: string, ...info: any[]) {
       if (!isLogLevelOneOf(this.logLevel, [LOG_LEVELS.ALL, LOG_LEVELS.INFO])) return
 
       console.log(addPrefix(text), ...info)
     },
-    warn(text, ...info) {
+    warn(text: string, ...info: any[]) {
       if (!isLogLevelOneOf(this.logLevel, [LOG_LEVELS.ALL, LOG_LEVELS.WARN])) return
 
       console.warn(addPrefix(text), ...info)
     },
   }
-
-  return logger
 }
 
 export const imapRawLogger = createLogger({ prefix: 'imap-raw' })
