65eb8a1d7f2aa91bdae8c47faacf06a938a26ac4
foxhable <foxhable@gmail.com>
2024-10-20 21:17:02 +0300

    feat: parentsNames field to mailboxes method

--- src/main/methods/mailboxes/types.ts
+++ src/main/methods/mailboxes/types.ts
@@ -3,6 +3,7 @@ import type { MailboxAttribute } from '@/main/classes/Mailbox/types.js'
 export interface ParsedMailbox {
   attributes: MailboxAttribute[]
   delimiter: string
+  parentsNames: string[]
   name: string
   raw: string
 }
