04b793c4fc17878e84614e2800e62091c828b110
Sevastyan <foxhable@gmail.com>
2025-04-12 15:02:34 +0300

    feat(imap-raw): IMAPConnection type

---
+++ libs/imap-raw/src/types/connection.ts
@@ -0,0 +1,5 @@
+import { EventEmitter } from "events";
+
+export interface IMAPConnection extends EventEmitter {
+  write: (buffer: string | Uint8Array) => boolean
+}
--- libs/imap-raw/src/types/index.ts
+++ libs/imap-raw/src/types/index.ts
@@ -1,3 +1,4 @@
 export * from './general.js'
 export * from './methods.js'
 export * from './response.js'
+export * from './connection.js'
