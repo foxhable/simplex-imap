c203e01042967b58064993f80f9a6e410fd061f9
Sevastyan <foxhable@gmail.com>
2025-04-12 15:02:41 +0300

    chore(ten-imap): add jest config

--- libs/ten-imap/jest.config.js
+++ libs/ten-imap/jest.config.js
@@ -1,5 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
+  transform: {
+    ".ts": [
+      "ts-jest",
+      {
+        isolatedModules: true,
+        useESM: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 }
