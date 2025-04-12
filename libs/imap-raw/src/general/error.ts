d1a66aa65e02da5b98f2ae53d01819ee4fd32c76
Sevastyan <foxhable@gmail.com>
2025-04-12 15:02:47 +0300

    feat: 'data' prop to IMAP errors

--- libs/imap-raw/src/general/error.ts
+++ libs/imap-raw/src/general/error.ts
@@ -1,9 +1,12 @@
 import { imapRawLogger } from 'logger'
 
 export class RawIMAPError extends Error {
+  public data: any[]
+
   constructor(message: string, ...data: any[]) {
     super(data ? 'Error from raw-imap lib. Detail above' : message)
     this.name = 'RawIMAPError'
+    this.data = data
     if (data) imapRawLogger.err(message, ...data)
   }
 }
