b1e07fdc978848fb2057fb3105a2155786d5e201
Sevastyan <foxhable@gmail.com>
2025-04-12 15:03:17 +0300

    chore: replace @typescript-eslint/no-unused-vars on strict rules in typescript

--- eslint.config.js
+++ eslint.config.js
@@ -9,4 +9,8 @@ export default tseslint.config(...tseslint.configs.recommended, importPlugin.fla
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
 })
