0c6b2d4094ad9ebea7b530a0408a6f99101a8c7f
Sevastyan <foxhable@gmail.com>
2025-04-12 15:02:56 +0300

    fix: type error in base imap parser

--- src/base/functions/parser.ts
+++ src/base/functions/parser.ts
@@ -2,12 +2,19 @@ import type { IMAPResponseLine, IMAPResult, IMAPTag, ResponseCode, ResponseStatu
 import { RESPONSE_CODES, RESPONSE_STATUSES } from '@/base/types/index.js'
 import { RawIMAPError } from '@/base/general/error.js'
 
-const TAG_REGEX_PART = '(?<tag>[\\d*]+)'
-const STATUS_REGEX_PART = `(?<status>${Object.values(RESPONSE_STATUSES).join('|')})`
-const CODE_REGEX_PART = `(?:\\[(${Object.values(RESPONSE_CODES).join('|')})] )?`
-const BODY_REGEX_PART = '(.*)'
+const GROUP_NAMES = {
+  TAG: 'tag',
+  STATUS: 'status',
+  CODE: 'code',
+  BODY: 'body',
+}
+
+const TAG_REGEX_PART = `(?<${GROUP_NAMES.TAG}>[\\d*]+)`
+const STATUS_REGEX_PART = `(?<${GROUP_NAMES.STATUS}>${Object.values(RESPONSE_STATUSES).join('|')})`
+const CODE_REGEX_PART = `(?:\\[(?<${GROUP_NAMES.CODE}>${Object.values(RESPONSE_CODES).join('|')})] )?`
+const BODY_REGEX_PART = `(?<${GROUP_NAMES.BODY}>.*)`
 export const IMAP_RESULT_REGEX = new RegExp(`(?:\r\n)?${TAG_REGEX_PART} ${STATUS_REGEX_PART} ${CODE_REGEX_PART}${BODY_REGEX_PART}\r\n$`)
-export const IMAP_RESPONSE_LINE_REGEX = new RegExp(`([*+]) (.+)`)
+export const IMAP_RESPONSE_LINE_REGEX = new RegExp(`(?<${GROUP_NAMES.TAG}>[*+]) (?<${GROUP_NAMES.BODY}>.+)`)
 
 export function parseIMAPResponse(data: string): IMAPResult {
   const match = data.match(IMAP_RESULT_REGEX)
@@ -38,16 +45,35 @@ export function parseIMAPResponse(data: string): IMAPResult {
       }
     })
 
-  return {
-    tag: match[1],
-    status: match[2] as ResponseStatus,
-    ok: (match[2] as ResponseStatus) === 'OK',
-    code: match[3] as ResponseCode || null,
+  if (!match.groups) {
+    throw new RawIMAPError('')
+  }
+
+  const status = match.groups[GROUP_NAMES.STATUS] as ResponseStatus
+
+  const result = {
+    tag: match.groups[GROUP_NAMES.TAG],
+    code: match.groups[GROUP_NAMES.CODE] as ResponseCode || null,
     raw: match[0].replaceAll('\r\n', ''),
-    body: match[4],
+    body: match.groups[GROUP_NAMES.BODY],
     response: {
       lines: responseLines,
       raw: response,
     },
+  }
+
+  // if statement because type error
+  if (status === 'NO' || status === 'BAD') {
+    return {
+      ok: false,
+      status,
+      ...result,
+    }
+  } else {
+    return {
+      ok: true,
+      status,
+      ...result,
+    }
   }
 }
