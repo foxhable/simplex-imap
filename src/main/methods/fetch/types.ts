30da5fa9232618f2365280e268d7e63678bd6072
Sevastyan <foxhable@gmail.com>
2025-04-12 15:03:35 +0300

    feat: fetch config parser with tests

---
+++ src/main/methods/fetch/types.ts
@@ -0,0 +1,61 @@
+export const FETCH_MACROS = {
+  ALL: 'ALL',
+  FAST: 'FAST',
+  FULL: 'FULL',
+} as const
+
+export type FetchMacro = (typeof FETCH_MACROS)[keyof typeof FETCH_MACROS]
+
+type SectionPart = number | string
+type SectionPartObject = { sectionPart?: SectionPart }
+type PartialSlice = { partial?: [number, number] | { from: number; to?: number } | number }
+export type SectionAndPartialSlice = SectionPartObject & PartialSlice
+
+export const HEADER_FIELDS_NAMES = {
+  ORIG_DATE: 'Date',
+  FROM: 'From',
+  SENDER: 'Sender',
+  REPLY_TO: 'Reply-To',
+  TO: 'To',
+  CC: 'Cc',
+  BCC: 'Bcc',
+  MESSAGE_ID: 'Message-ID',
+  IN_REPLY_TO: 'In-Reply-To',
+  REFERENCES: 'References',
+  SUBJECT: 'Subject',
+  COMMENTS: 'Comments',
+  KEYWORDS: 'Keywords',
+  RESENT_DATE: 'Resent-Date',
+  RESENT_FROM: 'Resent-From',
+  RESENT_SENDER: 'Resent-Sender',
+  RESENT_TO: 'Resent-To',
+  RESENT_CC: 'Resent-Cc',
+  RESENT_BCC: 'Resent-Bcc',
+  RESENT_MSG_ID: 'Resent-Message-ID',
+  RETURN: 'Return-Path',
+  RECEIVED: 'Received',
+} as const
+
+export type HeaderFieldName = (typeof HEADER_FIELDS_NAMES)[keyof typeof HEADER_FIELDS_NAMES]
+
+export type FetchDataItem = {
+  body?: boolean | SectionAndPartialSlice
+  bodyHeader?: boolean | SectionAndPartialSlice
+  bodyHeaderFields?: { fieldNames: Array<string | HeaderFieldName>; not?: boolean } & SectionAndPartialSlice
+  bodyMime?: Required<SectionPartObject> & PartialSlice
+  bodyText?: boolean | SectionAndPartialSlice
+  bodyStructure?: boolean
+  envelope?: boolean
+  flags?: boolean
+  internaldate?: boolean
+  size?: boolean
+  uid?: boolean
+}
+
+export type FetchConfig =
+  | FetchMacro
+  | FetchDataItem
+  | FetchDataItem[]
+  | {
+      raw: string
+    }
