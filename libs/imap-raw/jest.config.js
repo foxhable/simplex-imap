d70cf9d446b49ec25f60451e29bb0f78fb22adba
Sevastyan <foxhable@gmail.com>
2025-04-12 15:02:31 +0300

    chore(jest): fix jest config

--- libs/imap-raw/jest.config.js
+++ libs/imap-raw/jest.config.js
@@ -1,5 +1,13 @@
+import presets from 'ts-jest/presets/index.js'
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
+  transform: {
+    ...presets.jsWithTsESM.transform,
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 }
